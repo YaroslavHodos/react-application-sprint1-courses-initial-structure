a04898ac9104bf53b74f808bfab1d5c09bf76853
Yaroslav <sewa130391@gmail.com>
2022-06-02 18:32:09 +0300

    HW#47

--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -3,14 +3,27 @@ import { Reducer } from "react";
 import { coursesService } from "../config/service-config";
 import { ClientData, emptyClientData } from "../models/ClientData";
 import { Course } from "../models/Course";
-import {  AUTH_ACTION, SET_COURSES_ACTION} from "./actions";
-
+import { OperationCode } from "../models/OperationCode";
+import {  AUTH_ACTION, OPERATION_CODE_ACTION, SET_COURSES_ACTION} from "./actions";
+export const CLIENT_DATA_ITEM = "client-data"
 export const coursesReducer:Reducer<Course[], PayloadAction<Course[]>> =
  (courses = [], action):Course[] => {
      return action.type === SET_COURSES_ACTION ? action.payload : courses;
 }
 export const clientDataReducer: Reducer<ClientData, PayloadAction<ClientData>> = 
-(clientData = emptyClientData, action): ClientData => {
+(clientData = localStorage.getItem(CLIENT_DATA_ITEM)?
+ JSON.parse(localStorage.getItem(CLIENT_DATA_ITEM) as string) : emptyClientData, action): ClientData => {
    
-    return action.type === AUTH_ACTION ? action.payload : clientData;
+    if (action.type === AUTH_ACTION) {
+        localStorage.setItem(CLIENT_DATA_ITEM, JSON.stringify(action.payload));
+        return action.payload;
+    }
+    return clientData;
+}
+export const operationCodeReducer: Reducer<OperationCode, PayloadAction<OperationCode>> = 
+(operationCode = OperationCode.OK, action): OperationCode => {
+    if (action.type === OPERATION_CODE_ACTION) {
+        return action.payload;
+    }
+    return operationCode;
 }
